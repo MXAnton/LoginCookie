ada348242709dc72846e6b18aa28fe86ac82ec63
Anton Lehrberg <anton.lehrberg@guteskolan.se>
2023-01-16 12:14:12 +0100

    added logged in cookie, with login/logout function

---
+++ main.js
@@ -0,0 +1,62 @@
+// const wrapper = document.querySelector('main');
+const loggedInText = document.querySelector('#logged-in-text');
+const loginLogoutBtn = document.querySelector('#login-logout-btn');
+
+loginLogoutBtn.addEventListener("click", () => {
+    console.log(getCookie('isLoggedIn'));
+    // on btn click
+    if (getCookie('isLoggedIn') != null) {
+        deleteCookie('isLoggedIn');
+        logout();
+    } else {
+        setCookie('isLoggedIn', true, 5);
+        login();
+    }
+});
+
+
+setLoginLogoutState();
+function setLoginLogoutState() {
+    if (getCookie('isLoggedIn') != null) {
+        login();
+    } else {
+        logout();
+    }
+}
+
+
+function login() {
+    loggedInText.textContent = 'Logged in :)';
+    loginLogoutBtn.textContent = 'Logout';
+
+
+}
+function logout() {
+    loggedInText.textContent = 'Logged out :(';
+    loginLogoutBtn.textContent = 'Login';
+}
+
+
+
+// Cookie functions
+function setCookie(name, value, minutesToLive){
+    const date = new Date();
+    date.setTime(date.getTime() +  (minutesToLive * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`
+}
+function deleteCookie(name){
+    setCookie(name, null, null);
+}
+function getCookie(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+    
+    cArray.forEach(element => {
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1)
+        }
+    })
+    return result;
+}
